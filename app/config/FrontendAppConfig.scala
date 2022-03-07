0ee34c2ea99fa9408299fe76b2a487cc4e7990fe
Mark Reynolds <20478032+ers2015112@users.noreply.github.com>
2022-03-07 10:01:25 +0000

    DAC6-1508-cleanup: change log message (#169)

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -64,8 +64,6 @@ class FrontendAppConfig @Inject() (configuration: Configuration, servicesConfig:
 
   lazy val sendEmailUrl: String = configuration.get[Service]("microservice.services.email").baseUrl
 
-  lazy val emailFailureAlertMessage = "The email could not be sent to the EMAIL service"
-
   lazy val sendEmailToggle: Boolean       = configuration.get[Boolean]("features.send-email")
   lazy val contactDetailsToggle: Boolean  = configuration.get[Boolean]("contactDetailsToggle")
   lazy val manualJourneyToggle: Boolean   = configuration.get[Boolean]("manualJourneyToggle")
